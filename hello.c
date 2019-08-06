5c1cfa9d7e2bef679abd8c3bf806c91e36522cd2
linjianyu <jianyu.lin@altenchina.com>
2019-08-06 11:33:13 +0800

    add printf function in hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,19 @@
-
+#include <stdarg.h>
 
 void hello(void)
 {
 	printf("%s","hello world!");
 }
+
+void printf(const char *fmt,...)
+{
+	va_list args;
+	int n;
+	va_start(args, fmt);
+	n = vsprintf(sprint_buf, fmt, args);
+	va_end(args);
+	write(stdout, sprint_buf, n);
+	return n;
+}
+
+
